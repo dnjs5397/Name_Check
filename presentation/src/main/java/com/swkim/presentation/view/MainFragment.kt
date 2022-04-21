3dd4ebbd53af034413bd5b2942ec1ec736f351fd
Seungwon <dnjs5397@naver.com>
2022-04-21 23:31:39 +0900

    Fragment간 이동 구현

--- presentation/src/main/java/com/swkim/presentation/view/MainFragment.kt
+++ presentation/src/main/java/com/swkim/presentation/view/MainFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.swkim.presentation.R
 import com.swkim.presentation.base.BaseFragment
 import com.swkim.presentation.databinding.FragmentMainBinding
@@ -12,21 +13,13 @@ import com.swkim.presentation.databinding.FragmentMainBinding
 
 class MainFragment : BaseFragment<FragmentMainBinding>(R.layout.fragment_main) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
-    }
 
     override fun init() {
+        binding.fragment = this
     }
 
+    fun startBtnClick(view : View) {
+        this.findNavController().navigate(R.id.action_mainFragment_to_womanNameFragment)
+    }
 
 }
--- presentation/src/main/java/com/swkim/presentation/view/SplashActivity.kt
+++ presentation/src/main/java/com/swkim/presentation/view/SplashActivity.kt
@@ -3,10 +3,11 @@ package com.swkim.presentation.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.swkim.presentation.R
+import com.swkim.presentation.base.BaseActivity
+import com.swkim.presentation.databinding.ActivitySplashBinding
 
-class SplashActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+class SplashActivity : BaseActivity<ActivitySplashBinding>(R.layout.activity_splash) {
+    override fun init() {
+
     }
 }
